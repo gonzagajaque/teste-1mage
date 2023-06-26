1fd4d346b9745279b651089fbfdb271be945f08f
Jaqueline Gonzaga <jaqueline_gonzaga@hotmail.com>
2023-06-25 22:32:41 -0400

    feat: add styled components

---
+++ src/theme/index.ts
@@ -0,0 +1,23 @@
+export default {
+    COLORS: {
+        PRIMARY: '#3EB489',
+        ALL: '#8969E2',
+        CAMPAIGNS: '#259EA4',
+        DONATE_GOODS: '#3491E6',
+        CHARITY: '#FF9900',
+        EVENTS: '#F75B7E',
+        VOLUNTEERS: '#5D9EFB',
+        TEXT: '#000000',
+        TEXT_LIGHT: '#696969',
+        INPUT: '#a9a9a9',
+        WHITE: '#FFFFFF',
+        BACKGROUND: '#fbfbfb',
+    },
+    FONTS: {
+      REGULAR: 'Poppins_400Regular',
+      MEDIUM: 'Poppins_500Medium',
+      SEMIBOLD: 'Poppins_600SemiBold',
+      BOLD: 'Poppins_800ExtraBold',
+    }
+  };
+  
